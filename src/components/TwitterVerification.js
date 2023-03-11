e22c62083713a03f01e12f80d6322709b77b7284
sha237 <sha237@proton.me>
2023-03-11 16:16:27 +0100

    feat: add tailwind

--- src/components/TwitterVerification.js
+++ src/components/TwitterVerification.js
@@ -1,11 +1,72 @@
 import { useEffect } from 'react';
 import { useTwitter } from '../providers/Twitter.js';
 
+const categories = [
+  {
+    name: 'Developer',
+    subCategories: [
+      'Blockchain architecture',
+      'Distributed systems',
+      'Wallets',
+      'Tooling',
+      'DevOps',
+      'Frontend development',
+      'Backend development',
+      'Security',
+      'Risk management',
+      'Network operations',
+      'Infrastructure',
+    ],
+  },
+  { name: 'Developer relations', subCategories: [] },
+  { name: 'Product', subCategories: [] },
+  { name: 'Project management', subCategories: [] },
+  { name: 'Business development', subCategories: [] },
+  { name: 'Operations', subCategories: [] },
+  { name: 'Finance', subCategories: [] },
+  {
+    name: 'Data Science',
+    subCategories: [
+      'Data Visualization',
+      'Data analysis',
+      'Statistical Analysis',
+      'Natural Language Processing',
+      'Machine Learning',
+      'Big Data',
+      'Data Cleaning and Preprocessing',
+      'Data Modeling',
+    ],
+  },
+  { name: 'Creative', subCategories: [] },
+  { name: 'Marketing', subCategories: [] },
+  { name: 'Community engagement', subCategories: [] },
+  {
+    name: 'Meme maker',
+    subCategories: [
+      'Humor',
+      'Creativity',
+      'Timeliness',
+      'Shareability',
+      'Community engagement',
+      'Brand awareness',
+      'Memetic appeal',
+      'Tone',
+      'Visual design',
+      'Memetic longevity',
+    ],
+  },
+  { name: 'MathJournalist', subCategories: [] },
+  { name: 'Founder', subCategories: [] },
+  { name: 'Researcher', subCategories: [] },
+];
+
 const LoggedInView = () => {
   const { getUserInfo, userInfo, logout } = useTwitter();
 
   useEffect(() => {
-    getUserInfo();
+    (async () => {
+      const twitterUser = await getUserInfo();
+    })();
   }, []);
 
   return (
