c22fad8158d269c6448aaf3703e429d7795b5cd8
pentcle <pentacle@pentacle.xyz>
2023-03-12 11:22:46 +0000

    feat: colours

--- src/components/elements/PageHeading.js
+++ src/components/elements/PageHeading.js
@@ -2,7 +2,7 @@ const PageHeading = ({heading}) => {
     return (
         <article className="px-2 sm:px-4 sm:pb-4 mx-auto max-w-7xl">
             <div className="col-span-12 mt-2 sm:mt-4 text-center">
-                <h1 className={"py-4 md:text-6xl capitalize"}>{heading}</h1>
+                <h1 className={"py-4 text-xs"}>{heading}</h1>
             </div>
         </article>
     );
