c59ad7b60f9486ddf714d0f91f385c0b0e6aad35
sha237gh <118625539+sha237gh@users.noreply.github.com>
2023-03-13 17:48:00 +0100

    feat: use popper for popover (#51)
    Co-authored-by: pentacle <pentacle@pentacle.xyz>

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,15 +1,19 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { twitterUserInfoState, useTwitter } from "../../providers/Twitter.js";
+import { useTwitter, twitterUserInfoState } from "../../providers/Twitter.js";
 import { Popover } from "@headlessui/react";
 import { useRecoilValue } from "recoil";
 import { ensNameState } from "../../providers/Ens.js";
 import Link from "next/link.js";
+import { useState } from "react";
+import { usePopper } from "react-popper";
 
 const Header = () => {
   const { logout, login } = useTwitter();
   const ownEnsName = useRecoilValue(ensNameState);
   const userInfo = useRecoilValue(twitterUserInfoState);
-
+  let [referenceElement, setReferenceElement] = useState();
+  let [popperElement, setPopperElement] = useState();
+  let { styles, attributes } = usePopper(referenceElement, popperElement);
   return (
     <header className="flex flex-row justify-between items-center px-4 py-2 h-24 border-t border-b bg-tertiary border-primary text-txt-inverted dotted-01">
         <Link href={"/"}><h1 className="text-xs">STRT CRED</h1></Link>
@@ -26,7 +30,7 @@ const Header = () => {
           {userInfo && (
             <>
               <Popover className="relative">
-                <Popover.Button className={"w-24 h-full"}>
+                <Popover.Button className={"w-24 h-full"} ref={setReferenceElement}>
                   {" "}
                   <div className={"flex flex-col justify-center items-center"}>
                     <img className={"w-12 h-12 rounded-3xl border"}
@@ -37,7 +41,11 @@ const Header = () => {
                   </div>
                 </Popover.Button>
 
-                <Popover.Panel className="overflow-hidden absolute z-10 rounded shadow-lg">
+                <Popover.Panel className="overflow-hidden absolute z-10 rounded shadow-lg"
+                  ref={setPopperElement}
+                  style={styles.popper}
+                  {...attributes.popper}
+                >
                   <div className="grid bg-white">
                     <span className="px-4 py-2 text-xs cursor-pointer text-primary" onClick={() => logout()}>
                       Disconnect twitter
