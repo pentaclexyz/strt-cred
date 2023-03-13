38a55fcc8254ebf28e656f03f6569933cf01819c
pentcle <pentacle@pentacle.xyz>
2023-03-13 20:17:27 +0000

    feat: pages

---
+++ src/pages/ask/index.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from "react";
+import {NextSeo} from "next-seo";
+import Header from "../../components/layouts/Header";
+import {useRecoilValue} from "recoil";
+import {ensNameState} from "../../providers/Ens";
+import {useRouter} from "next/router";
+
+export default function Ask() {
+    const ensName = useRecoilValue(ensNameState);
+    const router = useRouter();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        (async () => {
+            if (!ensName) {
+                await router.push('/');
+            }
+            setMounted(true);
+        })()
+    }, []);
+
+    if (!mounted) {
+        return <></>
+    }
+
+    return (
+        <>
+            <NextSeo
+                title={`${name} . STRT CRED `}
+                description={`${name} on STRT CRED `}
+            />
+            <Header/>
+            <section className={"flex flex-col py-4 text-xs text-center"}>
+                hello
+            </section>
+        </>
+    );
+};
