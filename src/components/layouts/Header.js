394edf2213f24305ebfaed1ee9283eac276d814a
sha237 <sha237@proton.me>
2023-03-13 15:32:50 +0100

    feat: use popper for popover

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,12 +1,22 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useTwitter } from "../../providers/Twitter.js";
+import { useTwitter, twitterUserInfoState } from "../../providers/Twitter.js";
 import { Popover } from "@headlessui/react";
-import Link from "next/link";
+import { useRecoilValue } from "recoil";
+import { ensNameState } from "../../providers/Ens.js";
+import Link from "next/link.js";
+import { useState } from "react";
+import { usePopper } from "react-popper";
 
-const Header = ({ ownEnsName }) => {
-  const { userInfo, logout, login } = useTwitter();
+const Header = () => {
+  const { logout, login } = useTwitter();
+  const ownEnsName = useRecoilValue(ensNameState);
+  const userInfo = useRecoilValue(twitterUserInfoState);
+  let [referenceElement, setReferenceElement] = useState();
+  let [popperElement, setPopperElement] = useState();
+  let { styles, attributes } = usePopper(referenceElement, popperElement);
+
   return (
-    <header className="flex flex-row bg-tertiary justify-between items-center h-24 py-2 px-4 border-b border-t border-primary text-txt-inverted dotted-01">
+    <header className="flex flex-row justify-between items-center px-4 py-2 h-24 border-t border-b bg-tertiary border-primary text-txt-inverted dotted-01">
         <Link href={"/"}><h1 className="text-xs">STRT CRED</h1></Link>
       {ownEnsName ? (
         <div className="flex flex-row justify-items">
@@ -21,18 +31,22 @@ const Header = ({ ownEnsName }) => {
           {userInfo && (
             <>
               <Popover className="relative">
-                <Popover.Button className={"w-24 h-full"}>
+                <Popover.Button className={"w-24 h-full"} ref={setReferenceElement}>
                   {" "}
                   <div className={"flex flex-col justify-center items-center"}>
                     <img className={"w-12 h-12 rounded-3xl border"}
                       src={userInfo.profileImage.replace("_normal", "_bigger")}
                       alt={userInfo.name}
                     />
-                    <div className="mt-2 max-w-full truncate text-xs">{userInfo.name}</div>
+                    <div className="mt-2 max-w-full text-xs truncate">{userInfo.name}</div>
                   </div>
                 </Popover.Button>
 
-                <Popover.Panel className="overflow-hidden absolute z-10 rounded shadow-lg">
+                <Popover.Panel className="overflow-hidden absolute z-10 rounded shadow-lg"
+                  ref={setPopperElement}
+                  style={styles.popper}
+                  {...attributes.popper}
+                >
                   <div className="grid bg-white">
                     <span className="px-4 py-2 text-xs cursor-pointer text-primary" onClick={() => logout()}>
                       Disconnect twitter
